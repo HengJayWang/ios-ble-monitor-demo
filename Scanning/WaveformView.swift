9103bc7ee1a3a6962af8ca1d500b258395ee69e9
HengJay <hengjay@HengJays-Mac.local>
2018-01-30 11:19:45 +0800

    iOS BLE Monitor 2018/01/25

---
+++ Scanning/WaveformView.swift
@@ -0,0 +1,130 @@
+//
+//  WaveformView.swift
+//  ReadCharacteristic
+//
+//  Created by HengJay on 2018/1/19.
+//  Copyright © 2018年 Adonis Gaitatzis. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class WaveformView: UIView {
+
+    private struct Parameters {
+        static let backgroundOutlineWidth: CGFloat = 6.0
+        static let middleSeperateLineWidth: CGFloat = 1.5
+        static let signalLineWidth: CGFloat = 4.0
+    }
+    
+    var signal1Data: [CGFloat] = Array(repeating: CGFloat(UINT16_MAX/2), count: 1000) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    var signal1Index: Int = 0
+
+    var signal2Data: [CGFloat] = Array(repeating: CGFloat(UINT16_MAX/2), count: 1000) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    var signal2Index: Int = 0
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        drawWaveformBackground(rect: rect)
+        
+        drawSignal(rect: rect)
+    }
+    
+    func drawWaveformBackground (rect: CGRect) {
+        // set black background
+        let background = UIBezierPath(rect: rect)
+        UIColor.black.setFill()
+        background.fill()
+        
+        // draw gray middle seperate line
+        let seperateLine = UIBezierPath()
+        //set the path's line width to the height of the stroke
+        seperateLine.lineWidth = Parameters.middleSeperateLineWidth
+        
+        // move the initial point of the path
+        // to the start of the horizontal stroke
+        seperateLine.move(to: CGPoint(x: 0, y: bounds.height / 2))
+        
+        // add a point to the path at the end of the stroke
+        seperateLine.addLine(to: CGPoint(x: bounds.width,y: bounds.height / 2))
+        UIColor.gray.setStroke()
+        seperateLine.stroke()
+        
+        // draw outline of waveform area
+        let outline = UIBezierPath(rect: rect)
+        outline.lineWidth = Parameters.backgroundOutlineWidth
+        UIColor.green.setStroke()
+        outline.stroke()
+        
+    }
+    
+    func drawSignal (rect: CGRect) {
+        
+        let graphWidth: CGFloat = 0.9  // Graph is 90% of the width of the view
+        let width = bounds.width
+        let height = bounds.height
+        let stepX = (width * graphWidth) / CGFloat(signal1Data.count - 1)
+        
+        //draw waveform of signalECG
+        let origin = CGPoint(x: width * (1 - graphWidth) / 2, y: height * 0.45 )
+        
+        let signal1 = UIBezierPath()
+        signal1.lineWidth = 1.5
+        signal1.move(to: CGPoint(x: origin.x,
+                                   y: origin.y - height*0.4*(signal1Data[0]/CGFloat(UINT16_MAX))))
+        
+        for i in 1...signal1Data.count-1 {
+            signal1.addLine(to: CGPoint(x: origin.x + stepX * CGFloat(i),
+                                       y: origin.y - height*0.4*(signal1Data[i]/CGFloat(UINT16_MAX))))
+        }
+        UIColor.cyan.setStroke()
+        signal1.stroke()
+        
+        //draw waveform of signalPPG
+        let origin2 = CGPoint(x: width * (1 - graphWidth) / 2, y: height * 0.95 )
+        
+        let signal2 = UIBezierPath()
+        signal2.lineWidth = 1.5
+        signal2.move(to: CGPoint(x: origin2.x,
+                                   y: origin2.y - height*0.4*(signal2Data[0]/CGFloat(UINT16_MAX))))
+        
+        for i in 1...signal2Data.count-1 {
+            signal2.addLine(to: CGPoint(x: origin2.x + stepX * CGFloat(i),
+                                          y: origin2.y - height*0.4*(signal2Data[i]/CGFloat(UINT16_MAX))))
+        }
+        UIColor.red.setStroke()
+        signal2.stroke()
+
+    }
+    
+    func pushSignal1 (newValue: CGFloat) {
+        signal1Data.append(newValue)
+        signal1Data.removeFirst()
+    }
+    
+    func pushSignal1BySliding (newValue: CGFloat) {
+        signal1Data[signal1Index] = newValue
+        signal1Index = (signal1Index == signal1Data.count - 1) ? 0 : (signal1Index + 1)
+    }
+    
+    func pushSignal2 (newValue: CGFloat) {
+        signal2Data.append(newValue)
+        signal2Data.removeFirst()
+    }
+    
+    func pushSignal2BySliding (newValue: CGFloat) {
+        signal2Data[signal2Index] = newValue
+        signal2Index = (signal2Index == signal2Data.count - 1) ? 0 : (signal2Index + 1)
+    }
+    
+}
